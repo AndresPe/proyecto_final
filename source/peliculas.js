3e7e760e642ead898f3ea21f129d6c12a09885c0
Gear_Felipe_117 <julipe.andres@gmail.com>
2019-11-13 15:30:40 -0500

    Primer commit ensayo

---
+++ source/peliculas.js
@@ -0,0 +1,52 @@
+var express = require("express");
+var router = express.Router();
+
+//---IMPORTACIÓN DE LAS 405 PELICULAS
+var peliculas = require("./listado_peliculas");
+
+router.get("/pelicula/:nombre", (request, response)=>{
+  try {
+    let nombre = request.params.nombre
+    // console.log("este es el nombre: "+nombre);
+    let respuesta = {}
+    for (let index = 0; index < peliculas.length; index++) {
+      if (peliculas[index].nombre == nombre) {
+        respuesta.datos = peliculas[index]
+        respuesta.cantidad = 1
+        respuesta.mensaje = "Se encontró la pelicula exitosamente"
+        console.log(respuesta);
+        response.send(respuesta)
+      }
+    }
+    respuesta.datos = "error"
+    respuesta.cantidad = 0
+    respuesta.mensaje = "la pelicula no existe"
+    response.send(respuesta)
+
+  } catch (error) {
+    console.log(error);
+    response.status(400).send(error)
+  }
+
+})
+
+router.get("/peliculas", function(req, res, next) {
+  try {
+    //Impresión de las peliculas en consola
+    // console.log(peliculas);
+    console.log("Estoy en el get de peliculas");
+    
+    res.status(200).send({ mensaje: "Estoy en el get de peliculas" });
+  } catch (error) {
+    res.status(400).send({ mensaje: "Ha ocurrido un error!" });
+  }
+});
+
+router.delete("/peliculas", function(req, res, next) {
+
+  res.status(200).send({
+    mensaje: "ESTOY EN EL ENDPOINT DE DELETE"
+  });
+});
+
+module.exports = router;
