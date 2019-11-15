71cd45667167ed464404dae1ac671ace4f301231
AndresPe <julipe.andres@gmail.com>
2019-11-15 00:53:20 -0500

    Finalizados todos los puntos excepto listar peliculas de comedia, presenta error no identificado

--- source/peliculas.js
+++ source/peliculas.js
@@ -4,7 +4,8 @@ var router = express.Router();
 //---IMPORTACIÓN DE LAS 405 PELICULAS
 var peliculas = require("./listado_peliculas");
 
-router.get("/pelicula/:nombre", (request, response)=>{
+//BUSCAR PELICULA DADO EL NOMBRE
+router.get("/peliculas/:nombre", (request, response) => {
   try {
     let nombre = request.params.nombre
     // console.log("este es el nombre: "+nombre);
@@ -20,15 +21,15 @@ router.get("/pelicula/:nombre", (request, response)=>{
     }
     respuesta.datos = "error"
     respuesta.cantidad = 0
-    respuesta.mensaje = "la pelicula no existe"
-    response.send(respuesta)
+    respuesta.mensaje = "No se ha encontrado la pelicula"
+    response.status().send(respuesta)
   } catch (error) {
     console.log(error);
     response.status(400).send(error)
   }
 })
 
-router.get("/peliculas", function(req, res, next) {
+router.get("/peliculas", function(req, res) {
   try {
     //Impresión de las peliculas en consola
     // console.log(peliculas);
@@ -40,32 +41,93 @@ router.get("/peliculas", function(req, res, next) {
   }
 });
 
-router.delete("/peliculas", function(req, res, next) {
+//ELIMINAR PELICULAS QUE NO TIENEN GENERO DEFINIDO
+router.delete("/peliculas", function(request, response) {
+  let respuesta = {}
   try {
-    var newArrar = [] 
-    let indexNewArray = 0;
-    // console.log(peliculas[indexNewArray]);
-    for (let index = 0; index < peliculas.length; index++) {
-        if (peliculas[index].genero != "") {
-            newArrar[indexNewArray] = peliculas[index]
-            indexNewArray = indexNewArray + 1
-        }
+    let newArray = peliculas.filter(element => element.genero != "" && element.genero != null)
+    // console.log("array original");
+    // console.log(peliculas.length);
+    // console.log("array nuevo");
+    // console.log(newArray.length);
+    if (peliculas.length-newArray.length != 0) {
+      respuesta.estado = true
+      respuesta.cantidad = parseInt(peliculas.length)-parseInt(newArray.length)
+      respuesta.mensaje = "Peliculas sin genero eliminadas exitosamente"
+    } else {
+      respuesta.estado = true
+      respuesta.cantidad = 0
+      respuesta.mensaje = "La lista ya se encontraba filtrada"
     }
-  console.log("peliculas filtradas");
-  console.log(newArray);
-  response.status(200).send(newArrar)
-  res.status(200).send({
-    mensaje: "ESTOY EN EL ENDPOINT DE DELETE"
-  });
+    response.status(200).send(respuesta)
   } catch (error) {
+    respuesta.estado = false
+    respuesta.cantidad = 0
+    respuesta.mensaje = "Ha ocurrido un error"
+    respuesta.informacion = error
     console.log(error);
-    res.status(400).send({
-      mensaje: "error"
-    });
+    response.status(400).send(respuesta)
   }
-  
-  
-  
 });
+
+//CONSULTAR PELICULAS POR UBICACION
+router.get("/peliculas/ubicacion/:ubicacion", (request, response) => {
+  let respuesta = {}
+  try {
+    let ubicacion = request.params.ubicacion
+    let newArray = peliculas.filter(element => element.ubicacion == ubicacion)
+    if (newArray.length != 0) {
+      respuesta.estado = true
+      respuesta.datos = newArray
+      respuesta.cantidad = newArray.length
+      respuesta.mensaje = "Consulta realizada con éxito"
+    } else {
+      respuesta.estado = true
+      respuesta.cantidad = 0
+      respuesta.mensaje = "No se encontraron películas en la ubicación proporcionada"
+    }
+    response.status(200).send(respuesta)
+  } catch (error) {
+    respuesta.estado = false
+    respuesta.mensaje = "Ha ocurrido un error"
+    respuesta.error = error
+    response.status(400).send(respuesta)
+  }
+});
+
+//CONSULTAR PELICULAS ENTRE DOS AÑOS PASADOS POR PARAMETRO
+router.get("/peliculas/anio/:anio1/:anio2", (request, response) => {
+  let respuesta = {}
+  try {
+    let anio1 = parseInt(request.params.anio1) // SI NO PONGO EL PARSE E INGRESO LETRAS ME GENERA CONSULTA CORRECTAMENTE
+    let anio2 = parseInt(request.params.anio2)
+    let newArray = peliculas.filter(element => element.anio >= anio1 && element.anio <= anio2)
+    if (newArray.length != 0) {
+      respuesta.estado = true
+      respuesta.datos = newArray
+      respuesta.cantidad = newArray.length
+      respuesta.mensaje = "Se realizó la consulta de forma exitosa"
+    } else {
+      respuesta.estado = true
+      respuesta.cantidad = 0
+      respuesta.mensaje = "No se encontraron peliculas del año "+anio1+" al "+anio2
+    }
+    response.status(200).send(respuesta)
+  } catch (error) {
+    respuesta.estado = false
+    respuesta.mensaje = "Ha ocurrido un error al consultar"
+    respuesta.datos = error
+    response.status(400).send(respuesta)
+  }
+});
+
+router.get("/peliculas/comedia", (request, response, next) => {
+  // let newArray = {} //peliculas.filter(element => element.genero == "comedia")
+  //   console.log(newArra);
+  //   console.log("peliculas de comedia");
+  //   console.log(newArray.length);
+  //   response.status(200).send(newArray.length)
+  response.statusCode(200).send("respuesta") //ME GENERA ERROR ASÍ EL ENDPOINT ESTE VACIO TOTALMENTE, ERROR "ERR_HTTP_INVALID_STATUS_CODE"
+})
 
 module.exports = router;
